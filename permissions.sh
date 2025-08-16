2713b0433c6173859540fe329cce2968f3c35e59
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2025-08-16 13:21:24 -0400

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,3 +1,6 @@
 sudo chown -R www-data:www-data /var/www/html/upload_app /var/www/html/uploads
 sudo chmod -R 755 /var/www/html/upload_app /var/www/html/uploads
+sudo a2dissite 000-default
+sudo a2ensite upload_app
+sudo systemctl reload apache2
 sudo systemctl restart apache2  
